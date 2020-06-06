86078612e25b2cff0ddcdc2bb0fb5a7e8648cb02
linnefromice <linnefromice@gmail.com>
2020-06-06 22:39:35 +0900

    update rest_api_app - add test data

--- rest_api_app/controller.ts
+++ rest_api_app/controller.ts
@@ -1,6 +1,35 @@
 // Interface
 interface Movie {
-    id: string;
+    id: number;
     title: string;
     rating: number;
 }
+
+// data
+let movies: Array<Movie> = [
+    {
+        id: 1,
+        title: "Iron Man",
+        rating: 10,
+    },
+    {
+        id: 2,
+        title: "Incredible Hulk",
+        rating: 8
+    },
+    {
+        id: 3,
+        title: "Iron Man 2",
+        rating: 9
+    },
+    {
+        id: 4,
+        title: "Thor",
+        rating: 8
+    },
+    {
+        id: 5,
+        title: "Captain America: The First Avenger",
+        rating: 9
+    }
+];
