b8fc3b9fea97a870e295cde409d2e45d00a4cfac
linnefromice <linnefromice@gmail.com>
2022-01-17 23:38:05 +0900

    proceeded 'Getting the Deno version'

---
+++ examples.deno.land/cli/main.ts
@@ -0,0 +1,4 @@
+// Getting the Deno version
+console.log("Current Deno version", Deno.version.deno)
+console.log("Current TypeScript version", Deno.version.typescript)
+console.log("Current V8 version", Deno.version.v8)
