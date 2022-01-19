bc31108914ae968888a722daf8927d39058bd695
linnefromice <linnefromice@gmail.com>
2022-01-19 12:09:41 +0900

    progressed 'HTTP Requests'

--- examples.deno.land/http-requests/main.ts
+++ examples.deno.land/http-requests/main.ts
@@ -29,3 +29,43 @@ resp = await fetch(_url, {
 })
 console.log(await resp.text())
 // console.log(await resp.json())
+
+// `fetch` also accepts a `Request` object instead of URL + options
+const req = new Request("https://example.com", {
+  method: "DELETE",
+});
+resp = await fetch(req)
+
+const url = "https://example.com"
+new Request(url, {
+  method: "POST",
+  body: new Uint8Array([1,2,3])
+})
+new Request(url, {
+  method: "POST",
+  body: new Blob(["Hello, World!"])
+})
+new Request(url, {
+  method: "POST",
+  body: new URLSearchParams({ "foo": "bar" })
+})
+
+const formData = new FormData()
+formData.append("name", "Deno")
+formData.append("file", new Blob(["Hello, World"]), "hello.txt")
+resp = await fetch("https://example.com", {
+  method: "POST",
+  body: formData
+})
+
+// Fetch also supports streaming the request body.
+const bodyStream = new ReadableStream({
+  start(controller) {
+    controller.enqueue("Hello, World!")
+    controller.close()
+  }
+})
+resp = await fetch("https://example.com", {
+  method: "POST",
+  body: bodyStream
+})
